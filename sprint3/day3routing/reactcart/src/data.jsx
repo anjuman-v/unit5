9b0ea3a29686b72b33f84d03aafe14a55cb31f05
Anjuman <ansa.anjuman00786@gmail.com>
2022-05-12 15:56:53 +0530

    1-1

---
+++ sprint3/day3routing/reactcart/src/data.jsx
@@ -0,0 +1,23 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Tshirt',
+      price: 1400,
+      image: 'https://m.media-amazon.com/images/I/81uDnWPK5ZL._UL1500_.jpg',
+    },
+    {
+      id: '2',
+      name: 'Thor Car',
+      price: 24000,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Mahindra_Thar_Photoshoot_At_Perupalem_Beach_%28West_Godavari_District%2CAP%2CIndia_%29_Djdavid.jpg/280px-Mahindra_Thar_Photoshoot_At_Perupalem_Beach_%28West_Godavari_District%2CAP%2CIndia_%29_Djdavid.jpg',
+    },
+    {
+      id: '3',
+      name: 'BMW bike',
+      price: 1000,
+      image: 'https://images.carandbike.com/bike-images/colors/bmw/r-1250-gs/bmw-r-1250-gs-light-white-silver.png?v=1625739471',
+    },
+  ],
+};
+export default data;
---
+++ sprint3/day4roution/reactcart2/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css';
+// import { Navbar } from "./components/Navbar"
+
+import { useState } from 'react';
+import { AllRoute } from './components/Router';
+
+function App() {
+  
+  return (
+ 
+
+
+<div>
+<AllRoute/>
+</div>
+
+
+
+  );
+}
+
+export default App;
